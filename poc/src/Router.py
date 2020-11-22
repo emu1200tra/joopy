be84efb31f49a0f468dbcaf251265736c336773d
xjshengchen <r08922066@ntu.edu.tw>
2020-11-22 16:17:05 +0800

    [Feature] Joopy start
    Owner: Sheng Chen
    
    Content: start function of Joopy
    
    Scope: Router
    
    Changes: Route module
    
    Reference:

--- poc/src/Router.py
+++ poc/src/Router.py
@@ -228,28 +228,480 @@ class Router(Registry):
     @abstractmethod
     def domainRouterSet(self, domain, body):
         """
-        /**
+        Enabled routes for specific domain. Domain matching is done using the <code>host</code> header.
+   
+        <pre>{@code
+            {
+                domain("foo.com", () -> {
+                    get("/", ctx -> "foo");
+                });
+                domain("bar.com", () -> {
+                    get("/", ctx -> "bar");
+                });
+            }
+        }</pre>
+   
+        NOTE: if you run behind a reverse proxy you might to enabled {@link #setTrustProxy(boolean)}.
+    
+        @param domain Predicate
+        @param body Route action.
+        @return This router.
+        """
+        pass
+    
+    def useRouter(self, predicate, router):
+        """
         Import routes from given router. Predicate works like a filter and only when predicate pass
         the routes match against the current request.
-    
+   
         Example of domain predicate filter:
-    
+   
         <pre>{@code
             {
                 use(ctx -> ctx.getHost().equals("foo.com"), new FooApp());
                 use(ctx -> ctx.getHost().equals("bar.com"), new BarApp());
             }
         }</pre>
-    
+   
         Imported routes are matched only when predicate pass.
-    
+   
         NOTE: if you run behind a reverse proxy you might to enabled {@link #setTrustProxy(boolean)}.
-    
+   
         NOTE: ONLY routes are imported. Services, callback, etc.. are ignored.
-    
+   
         @param predicate Context predicate.
         @param router Router to import.
         @return This router.
         @deprecated Use {@link #mount(Predicate, Router)}
         """
+        return self.mountRouter(predicate, router)
+    
+    @abstractmethod
+    def mountRouter(self, predicate, router):
+        """
+        Import routes from given router. Predicate works like a filter and only when predicate pass
+        the routes match against the current request.
+   
+        Example of domain predicate filter:
+   
+        <pre>{@code
+            {
+                use(ctx -> ctx.getHost().equals("foo.com"), new FooApp());
+                use(ctx -> ctx.getHost().equals("bar.com"), new BarApp());
+            }
+        }</pre>
+   
+        Imported routes are matched only when predicate pass.
+   
+        NOTE: if you run behind a reverse proxy you might to enabled {@link #setTrustProxy(boolean)}.
+   
+        NOTE: ONLY routes are imported. Services, callback, etc.. are ignored.
+   
+        @param predicate Context predicate.
+        @param router Router to import.
+        @return This router.
+        """
         pass
+    
+    def userRouterSet(self, predicate, body):
+        """
+        Import routes from given action. Predicate works like a filter and only when predicate pass
+        the routes match against the current request.
+    
+        Example of domain predicate filter:
+   
+        <pre>{@code
+            {
+                use(ctx -> ctx.getHost().equals("foo.com"), () -> {
+                    get("/", ctx -> "foo");
+                });
+                use(ctx -> ctx.getHost().equals("bar.com"), () -> {
+                    get("/", ctx -> "bar");
+                });
+            }
+        }</pre>
+    
+        NOTE: if you run behind a reverse proxy you might to enabled {@link #setTrustProxy(boolean)}.
+   
+        @param predicate Context predicate.
+        @param body Route action.
+        @return This router.
+        @deprecated Use {@link #mount(Predicate, Runnable)}
+        """
+        return self.mountRouterSet(predicate, body)
+    
+    @abstractmethod
+    def mountRouterSet(predicate, body):
+        """
+        Import routes from given action. Predicate works like a filter and only when predicate pass
+        the routes match against the current request.
+   
+        Example of domain predicate filter:
+   
+        <pre>{@code
+            {
+                mount(ctx -> ctx.getHost().equals("foo.com"), () -> {
+                    get("/", ctx -> "foo");
+                });
+                mount(ctx -> ctx.getHost().equals("bar.com"), () -> {
+                    get("/", ctx -> "bar");
+                });
+            }
+        }</pre>
+   
+        NOTE: if you run behind a reverse proxy you might to enabled {@link #setTrustProxy(boolean)}.
+   
+        NOTE: ONLY routes are imported. Services, callback, etc.. are ignored.
+   
+        @param predicate Context predicate.
+        @param body Route action.
+        @return This router.
+        """
+        pass
+    
+    def useRouterPath(self, path, router):
+        """
+        Import all routes from the given router and prefix them with the given path.
+   
+        NOTE: ONLY routes are imported. Services, callback, etc.. are ignored.
+   
+        @param path Prefix path.
+        @param router Router to import.
+        @return This router.
+        @deprecated Use {@link #mount(String, Router)}
+        """
+        return self.mount(path, router)
+    
+    @abstractmethod
+    def mountRouterPath(self, path, router):
+        """
+        Import all routes from the given router and prefix them with the given path.
+   
+        NOTE: ONLY routes are imported. Services, callback, etc.. are ignored.
+   
+        @param path Prefix path.
+        @param router Router to import.
+        @return This router.
+        """
+        pass
+    
+    def useRouterOnly(self, router):
+        """
+        Import all routes from the given router.
+   
+        NOTE: ONLY routes are imported. Services, callback, etc.. are ignored.
+   
+        @param router Router to import.
+        @return This router.
+        @deprecated Use {@link #mount(Router)}
+        """
+        return self.mountRouterOnly(router)
+    
+    @abstractmethod
+    def mountRouterOnly(self, router):
+        """
+        Import all routes from the given router.
+   
+        NOTE: ONLY routes are imported. Services, callback, etc.. are ignored.
+   
+        @param router Router to import.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def mvcRouter(self, router):
+        """
+        Import all route method from the given controller class. At runtime the controller instance
+        is resolved by calling {@link Jooby#require(Class)}.
+   
+        @param router Controller class.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def mvcT(self, router, provider):
+        """
+        Import all route method from the given controller class.
+   
+        @param router Controller class.
+        @param provider Controller provider.
+        @param <T> Controller type.
+        @return This router.    
+        """
+        pass
+    
+    @abstractmethod
+    def mvcObj(self, router):
+        """
+        Import all route methods from given controller instance.
+   
+        @param router Controller instance.
+        @return This routes.
+        """
+        pass
+    
+    @abstractmethod
+    def ws(self, pattern, handler):
+        """
+        Add a websocket handler.
+   
+        @param pattern WebSocket path pattern.
+        @param handler WebSocket handler.
+        @return A new route.
+        """
+        pass
+    
+    @abstractmethod
+    def sse(self, pattern, handler):
+        """
+        Add a server-sent event handler.
+   
+        @param pattern Path pattern.
+        @param handler Handler.
+        @return A new route.
+        """
+        pass
+    
+    @abstractmethod
+    def getRoutes(self):
+        """
+        Returns all routes.
+        
+        @return All routes.
+        """
+        pass
+    
+    @abstractmethod
+    def encoderRouter(self, encoder):
+        """
+        Register a route response encoder.
+   
+        @param encoder MessageEncoder instance.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def encoderRouterContent(self, contentType, encoder):
+        """
+        Register a route response encoder.
+   
+        @param contentType Accept header should matches the content-type.
+        @param encoder MessageEncoder instance.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def getTmpdir(self):
+        """
+        Application temporary directory. This method initialize the {@link Environment} when isn't
+        set manually.
+   
+        @return Application temporary directory.
+        """
+        pass
+    
+    @abstractmethod
+    def decoder(self, contentType, decoder):
+        """
+        Register a decoder for the given content type.
+   
+        @param contentType Content type to match.
+        @param decoder MessageDecoder.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def getWorker(self):
+        """
+        Returns the worker thread pool. This thread pool is used to run application blocking code.
+   
+        @return Worker thread pool.
+        """
+        pass
+    
+    @abstractmethod
+    def setWorker(self, worker):
+        """
+        Set a worker thread pool. This thread pool is used to run application blocking code.
+   
+        @param worker Worker thread pool.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def setDefaultWorker(self, worker):
+        """
+        Set the default worker thread pool. Via this method the underlying web server set/suggests the
+        worker thread pool that should be used it.
+   
+        A call to {@link #getWorker()} returns the default thread pool, unless you explicitly set one.
+   
+        @param worker Default worker thread pool.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def decoratorRouter(self, decorator):
+        """
+        Add a route decorator to the route pipeline.
+   
+        @param decorator Decorator.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def beforeRouter(self, before):
+        """
+        Add a before route decorator to the route pipeline.
+   
+        @param before Before decorator.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def afterRouter(self, after):
+        """
+        Add an after route decorator to the route pipeline.
+   
+        @param after After decorator.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def dispatch(self, body):
+        """
+        Dispatch route pipeline to the {@link #getWorker()} worker thread pool. After dispatch
+        application code is allowed to do blocking calls.
+   
+        @param body Dispatch body.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def dispatchExecutor(self, executor, body):
+        """
+        Dispatch route pipeline to the given executor. After dispatch application code is allowed to
+        do blocking calls.
+   
+        @param executor Executor. {@link java.util.concurrent.ExecutorService} instances automatically
+        shutdown at application exit.
+        @param body Dispatch body.
+        @return This router.
+        """
+        pass
+    
+    @abstractmethod
+    def routes(self, body):
+        """
+        Group one or more routes. Useful for applying cross cutting concerns to the enclosed routes.
+   
+        @param body Route body.
+        @return All routes created.
+        """
+        pass
+    
+    @abstractmethod
+    def path(self, pattern, body):
+        """
+        Group one or more routes under a common path prefix. Useful for applying cross cutting
+        concerns to the enclosed routes.
+   
+        @param pattern Path pattern.
+        @param body Route body.
+        @return All routes created.
+        """
+        pass
+    
+    def get(self, pattern, handler):
+        """
+        Add a HTTP GET handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(GET, pattern, handler)
+    
+    def post(self, pattern, handler):
+        """
+        Add a HTTP POST handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(POST, pattern, handler)
+    
+    def put(self, pattern, handler):
+        """
+        Add a HTTP PUT handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(PUT, pattern, handler)
+    
+    def delete(self, pattern, handler):
+        """
+        Add a HTTP DELETE handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(DELETE, pattern, handler)
+    
+    def patch(self, pattern, handler):
+        """
+        Add a HTTP PATCH handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(PATCH, pattern, handler)
+    
+    def head(self, pattern, handler):
+        """
+        Add a HTTP HEAD handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(HEAD, pattern, handler)
+    
+    def options(self, pattern, handler):
+        """
+        Add a HTTP OPTIONS handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(OPTIONS, pattern, handler)
+    
+    def trace(self, pattern, handler):
+        """
+        Add a HTTP TRACE handler.
+   
+        @param pattern Path pattern.
+        @param handler Application code.
+        @return A route.
+        """
+        return self.route(TRACE, pattern, handler)
+    
+    
+    
