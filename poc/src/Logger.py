0df61aa89ed2cee0c73ad613bc72f63e4bcb9cd3
root <root@LAPTOP-LGCDL4M5.localdomain>
2020-11-26 20:16:51 +0800

    [Bug Fix] Remove ; in logger
    Owner:
    Danny HHHung
    
    Content:
    Remove ; in logger.py
    
    Scope:
    logger
    
    Changes:
    LOgger.py
    
    Reference:

--- poc/src/Logger.py
+++ poc/src/Logger.py
@@ -5,7 +5,7 @@ class Logger(object):
 		error_class = e.__class__.__name__
 		detail = e.args[0]
 		print("------------error-------------")
-		print("{}".format(message));
+		print("{}".format(message))
 		print("error type: {}".format(error_class))
-		print("{}".format(detail));
+		print("{}".format(detail))
 		print("------------------------------")
