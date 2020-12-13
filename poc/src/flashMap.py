05734b6c3d2cc7d5016f23084adc0ec54456ac82
xjshengchen <r08922066@ntu.edu.tw>
2020-12-13 15:24:29 +0800

    [Feature] flashMap
    Owner: Sheng Chen
    
    Content: flashMap interface
    
    Scope: flashMap
    
    Changes: flashMap.py
    
    Reference:

---
+++ poc/src/flashMap.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+from context import context
+from cookie import cookie
+from flashMapImpl import flashMapImpl
+
+class flashMap(object):
+    """
+    Flash map attribute.
+    """
+    NAME = 'flash'
+
+    @staticmethod
+    def create(ctx: context, template: cookie) -> flashMap:
+        """
+        Creates a new flash-scope using the given cookie.
+   
+        @param ctx Web context.
+        @param template Cookie template.
+        @return A new flash map.
+        """
+        return flashMapImpl(context, template)
+
+    @abstractmethod
+    def keep(self) -> flashMap:
+        """
+        Keep flash cookie for next request.
+   
+        @return This flash map.
+        """
+        pass
