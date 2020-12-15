577adac2809c67562515cd7322943183c24cc1ed
seven <seven.tychi@gmail.com>
2020-12-15 14:59:21 +0800

    default context implementation

--- poc/src/defaultContext.py
+++ poc/src/defaultContext.py
@@ -31,6 +31,14 @@ class UrlParser(object):
     pass
 
 
+class HashValue(object):
+    pass
+
+
+class MediaType(object):
+    pass
+
+
 class DefaultContext(Context):
     def __init__(self):
         pass
@@ -70,7 +78,8 @@ class DefaultContext(Context):
         return self
 
     def flash(self):
-        return Context.get_attributes().computeIfAbsent(FlashMap.NAME, lambda key: FlashMap.create(self, Context.get_router().getFlashCookie().clone()))
+        return Context.get_attributes().computeIfAbsent(FlashMap.NAME, lambda key: FlashMap.create(self,
+                                                                                                   Context.get_router().getFlashCookie().clone()))
 
     @dispatch(str)
     def flash(self, name):
@@ -115,6 +124,89 @@ class DefaultContext(Context):
         value = Context.cookieMap().get(name)
         return Value.missing(name) if value is None else Value.value(self, name, value)
 
+    @dispatch(str)
     def path(self, name):
         value = Context.path_map().get(name)
         return MissingValue(name) if value is None else SingleValue(self, name, UrlParser.decode_path_segment(value))
+
+    @dispatch(object)
+    def path(self, class_type):
+        return Context.path().to(class_type)
+
+    @dispatch()
+    def path(self):
+        path = HashValue(self, None)
+        for entry in Context.path_map().entry_set():
+            path.put(entry.get_key(), entry.get_value())
+
+    @dispatch(str)
+    def query(self, name):
+        return self.query().get(name)
+
+    @dispatch(object)
+    def query(self, class_type):
+        return self.query().to(class_type)
+
+    def query_string(self):
+        return self.query().query_string()
+
+    def query_map(self):
+        return self.query().to_multimap()
+
+    def header(self, name):
+        return self.header().get(name)
+
+    def header_map(self):
+        return self.header().to_map()
+
+    def header_multimap(self):
+        return self.header().to_multimap()
+
+    def accept(self, content_type):
+        accept = Context.header("Accept")
+        return accept.is_missing() or content_type.matches(accept.value())
+
+    def accept(self, produce_types):
+        accept_types = MediaType.parse(Context.header("Accept").value_or_null())
+        result = None
+        for accept_type in accept_types:
+            for produce_type in produce_types:
+                if produce_type.matches(accept_type):
+                    if result is None:
+                        result = produce_type
+                    else:
+                        _max = MediaType.MOST_SPECIFIC.apply(result, produce_type)
+                        if _max is not result:
+                            result = _max
+
+        return result
+
+    @dispatch()
+    def get_request_url(self):
+        return self.get_request_url("")
+
+    @dispatch(str)
+    def get_request_url(self, path):
+        scheme = Context.get_scheme()
+        host = Context.get_host()
+        port = Context.get_port()
+        url = ""
+        url += scheme
+        url += "://"
+        url += host
+
+        if port > 0 and port is not 80 and port is not 443:
+            url += ":"
+            url += port
+
+        if path is None or len(path) == 0:
+            url += Context.get_request_path()
+        else:
+            context_path = Context.get_context_path()
+            if context_path != "/" and path.startswith(context_path):
+                url += context_path
+            url += path
+        url += Context.query_string()
+
+        return url
+
